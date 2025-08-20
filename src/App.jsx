f3e25e9dadd144fab7be3890fc3b35a1a3b6cf08
snadi043 <snadi043@uottawa.ca>
2025-08-20 11:55:26 -0400

    Refactored the files to execute all the steps in the jobs of the workflow.

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import logo from './assets/images/logo.png';
 function App() {
   return (
     <>
-      <header>
+      <header class="header">
         <div id="logo-img">
           <img src={logo} />
         </div>
+        <h1>
+          Learn React
+        </h1>
         <h1>Learn & Master GitHub Actions</h1>
       </header>
       <MainContent />
