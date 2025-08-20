48e199abd9d2826076d2fe011a25392a943bbd4a
snadi043 <snadi043@uottawa.ca>
2025-08-20 12:18:43 -0400

    Refactoring the test cases.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import MainContent from './components/MainContent';
 import logo from './assets/images/logo.png';
+import './index.css'
 
 function App() {
   return (
@@ -8,9 +9,7 @@ function App() {
         <div id="logo-img">
           <img src={logo} alt="logo-react"/>
         </div>
-        <h1>
-          Learn React
-        </h1>
+        <h1>Learn React</h1>
         <h1>Learn & Master GitHub Actions</h1>
       </header>
       <MainContent />
